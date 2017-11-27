d9c6dfbce008ebf0ab839cbbe19b886d7f1dc08c
liulinhui <1249055292@qq.com>
2017-11-27 17:16:47 +0800

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+
+shity = {
+	crypto : require("./lib/transactions/crypto.js"),
+	delegate : require("./lib/transactions/delegate.js"),
+	signature : require("./lib/transactions/signature.js"),
+	multisignature : require("./lib/transactions/multisignature.js"),
+	transaction : require("./lib/transactions/transaction.js"),
+	vote : require("./lib/transactions/vote.js"),
+	ipfs : require("./lib/transactions/ipfs.js"),
+	networks : require("./lib/networks.js"),
+	slots : require("./lib/time/slots.js"),
+
+	ECPair : require("./lib/ecpair.js"),
+	HDNode : require("./lib/hdnode.js"),
+	ECSignature : require("./lib/ecsignature.js")
+};
+
+// extra aliases for bitcoinlib-js compatibility
+var libCrypto = require('./lib/crypto.js');
+for (var method in libCrypto) {
+    shity.crypto[method] = libCrypto[method]
+}
+
+module.exports = shity;
