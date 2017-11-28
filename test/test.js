d5fd80b5a0748b38f0e7acbeb0e96630263b9be8
liulinhui <1249055292@qq.com>
2017-11-28 15:27:23 +0800

    update

---
+++ test/test.js
@@ -0,0 +1,5 @@
+var crypto=require('../lib/transactions/crypto');
+
+var secrete='middle spread recipe orphan grab under census middle camera vehicle master oval';
+
+console.log(crypto.getKeys(secrete).publicKey);
